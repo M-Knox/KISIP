9eda5164fa86ee2d8805027978fb52ab6537a376
M-Knox <mkamunya86@gmail.com>
2026-06-15 13:36:34 +0300

    Final update

--- hello python.py
+++ hello python.py
@@ -1,6 +1,9 @@
-import geopandas as gpd;
-import pandas as pd;
+import pandas as pd
+import geopandas as gpd
 
-head = pd.read_csv("data/kisip_zone_scmi_both.csv");
-print(head.columns.tolist())
-print(head.head(5))
+gdf = gpd.read_file("data/mukuru_zones_spatial.geojson")
+preds = pd.read_csv("data/kisip_mukuru_predictions.csv")
+
+print("GDF zone_ids:", gdf["zone_id"].head(5).tolist())
+print("Preds zone_ids:", preds["zone_id"].head(5).tolist())
+print("Matching zone_ids:", gdf["zone_id"].isin(preds["zone_id"]).sum(), "out of", len(gdf))
